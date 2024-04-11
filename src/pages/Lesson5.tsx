d101731589d55aae7c5d7e1a5f1c68b6436918b0
Alessandro Fiori <alefiori97@gmail.com>
2024-04-11 11:40:20 +0200

    fix: missing bracket lesson5 example

--- src/pages/Lesson5.tsx
+++ src/pages/Lesson5.tsx
@@ -360,7 +360,7 @@ if (x > 5) {
             {`var numbers = [1, 2, 3, 4, 5];
 var doubledNumbers = numbers.map(function(number) {
     return number * 2;
-}
+})
 console.log(doubledNumbers); // [2, 4, 6, 8, 10]`}
           </code>
         </pre>
